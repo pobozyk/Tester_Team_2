223532cc45cf0ef4fa8035c4c3d06833e39092cd
JarekGiergielewicz <jarek.giergielewicz@me.com>
2023-05-20 14:28:05 +0200

    add feature aperol spritz pierwszy gratis

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:")
+
 # Sprawdzamy czy podany wiek jest liczbą
 if wiek.isdigit() == False:
 	exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
+# sprawdzanie czy wybor płci jest poprawny
+if gender.upper() != 'K' and gender.upper() != 'M':
+	exit('podana płeć, powinno być K lub M!')
 wiek=int(wiek)
+if wiek >= 30 and gender.upper() == 'K':
+	print("Otrzymujesz aperol spritz pierwszy gratis!")
 if wiek>=18 and wiek<40:
 	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
 elif wiek>=40:
