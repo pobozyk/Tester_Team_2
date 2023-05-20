4dc6647197dc729778d631796a89c6f9576b131c
pobozyk <kasia.krajewska@gmail.com>
2023-05-20 18:53:13 +0000

    Function: wybor plci

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+def wybor_plci():
+	gender = input("Podaj plec: K/M")
+	if gender == "K":
+		print("woman")
+	elif gender == "M":
+		print("men")
+	else:
+		print("Wybor nieprawidlowy")
+
 wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:")
 # Sprawdzamy czy podany wiek jest liczbą
 if wiek.isdigit() == False:
@@ -5,8 +14,10 @@ if wiek.isdigit() == False:
 wiek=int(wiek)
 if wiek>=18 and wiek<40:
 	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
+	wybor_plci()
 elif wiek>=40:
 	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
 	print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
+	wybor_plci()
 else:
   exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
