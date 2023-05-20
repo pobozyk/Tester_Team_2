42f67f2b0c224a7faf03dd4341a536cba41b0c84
pobozyk <kasia.krajewska@gmail.com>
2023-05-20 20:08:56 +0000

    fix

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 def wybor_plci():
-	gender = input("Podaj plec: K/M")
+	gender = input("Podaj plec [K/M]:").upper()
 	if gender == "K":
 		print("woman")
 	elif gender == "M":
 		print("men")
 	else:
-		print("Wybor nieprawidlowy")
+		exit("Wybor nieprawidlowy")
+
+	return gender
 
 wiek = input("Podaj wiek użytkownika jako liczbe calkowitą:")
+gender = wybor_plci()
+
 # Sprawdzamy czy podany wiek jest liczbą
 if wiek.isdigit() == False:
 	exit("Wiek musi być liczbą albo podana liczba nie jest calkowita")
 wiek=int(wiek)
 if wiek>=18 and wiek<40:
 	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
-	wybor_plci()
 elif wiek>=40:
 	print("Witaj w naszej apce z alkoholem, zapraszamy do zakupów")
 	print("Uważaj w Twoim wieku nie przasadzaj ze spożyciem")
-	wybor_plci()
 else:
   exit("Jesteś za młoda/y na alkohol. Zapraszamy na disney.com")
